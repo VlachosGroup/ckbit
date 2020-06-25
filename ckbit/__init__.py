4b77199a98d4160ff73139277b4f92560759d970
Jeffrey Frey <jtfrey@users.noreply.github.com>
2020-06-25 16:01:09 -0400

    Update __init__.py

--- ckbit/__init__.py
+++ ckbit/__init__.py
@@ -10,7 +10,7 @@ name = 'ckbit'
 __version__ = '1.0.0'
 
 # Pull rxn_ord, pfr, cstr, app_ea in as ckbit.<x>:
-import .rxn_ord
-import .pfr
-import .cstr
-import .app_ea
+import rxn_ord
+import pfr
+import cstr
+import app_ea

c6fba2175f871c0934c4083c6d38195a70edebed
Jeffrey Frey <jtfrey@users.noreply.github.com>
2020-06-25 15:58:14 -0400

    Add import content to ckbit/__init__.py

---
+++ ckbit/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+####
+#
+# setuptools likes to see a name for the package,
+# and it's best-practices to have the __version__
+# present, too:
+#
+name = 'ckbit'
+__version__ = '1.0.0'
+
+# Pull rxn_ord, pfr, cstr, app_ea in as ckbit.<x>:
+import .rxn_ord
+import .pfr
+import .cstr
+import .app_ea
